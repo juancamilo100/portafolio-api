b16b3045b5ac0971cf8da70e55704f3468f6ab9c
JuanEspinosa <juancamilo100@hotmail.com>
2019-05-17 17:02:45 -0400

    Finished GET and POST for Users

--- src/controllers/portfolios.js
+++ src/controllers/portfolios.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
+const mongoose = require('mongoose');
 const { Portfolio } = require('../models/portfolio');
 
 router.get('/', async (req, res, next) => {
@@ -29,6 +30,7 @@ router.post('/', async (req, res, next) => {
 
     try {
         const newPortfolio = new Portfolio({
+            _id: mongoose.Types.ObjectId(),
             name: req.body.name,
             description: req.body.description,
             tickers: req.body.tickers
--- src/models/portfolio/schema.js
+++ src/models/portfolio/schema.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
 const schema = new Schema({
+    _id: {
+        type: ObjectId,
+        required: [true]
+    },
     name: {
         type: String,
         required: [true],
@@ -12,7 +16,11 @@ const schema = new Schema({
     },
     tickers: {
         type: Array      
+    },
+    user: {
+        type: ObjectId, 
+        ref: 'User'
     }
-});
+}, { _id: false });
 
 module.exports = { schema };
