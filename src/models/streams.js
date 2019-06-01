d3a5ab06555870a5df2319d9ed55863563988674
JuanEspinosa <juancamilo100@hotmail.com>
2019-06-01 13:17:45 -0400

    Added endpoint and synchronization with user

--- src/models/streams.js
+++ src/models/streams.js
@@ -1,13 +1,26 @@
 const { Portfolio } = require('./portfolio');
 const { User } = require('./user');
 
-const syncNewPortafolioToUser = async (data) => {
+const syncNewPortafolioToUser = async (data, operation) => {
     try {
         const user = await User.findById(data.fullDocument.user);
         const newPortfolios = user.portfolios.slice(0);
 
-        newPortfolios.push(data.fullDocument._id);
-        user.portfolios = newPortfolios;
+        switch (operation) {
+            case "insert":
+                newPortfolios.push(data.fullDocument._id);
+                user.portfolios = newPortfolios;
+                break;
+            
+            case "delete":
+                user.portfolios = newPortfolios.filter(
+                        (portfolioId) => !portfolioId === data.fullDocument._id
+                    );
+                break;                
+        
+            default:
+                break;
+        }
 
         await user.save();
     } catch (error) {
@@ -18,8 +31,8 @@ const syncNewPortafolioToUser = async (data) => {
 const initDatabaseStreams = () => {
     Portfolio.watch()
         .on('change', async (data) => { 
-            if (data.operationType === 'insert') {
-                syncNewPortafolioToUser(data);
+            if (['insert', 'delete'].indexOf(data.operationType) > -1) {
+                syncNewPortafolioToUser(data, data.operationType);
             }
         });
 }
