db6bb8684688767e451d02428883a12ae68913ba
Laura Artunduaga <laura.perea.artunduaga@gmail.com>
2019-06-04 10:07:05 -0400

    mocha test

--- test/usersController.test.js
+++ test/usersController.test.js
@@ -34,4 +34,20 @@ describe('users', () => {
       });
   });
 
+  /*
+  * Test the /GET user by id route
+  */
+ describe('/GET user', () => {
+    it('it should GET all the users', (done) => {
+      chai.request(server)
+          .get('/users/5cf1de2bf654b3686038dcee')
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(1);
+            done();
+          });
+    });
+});
+
 });
