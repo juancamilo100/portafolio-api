56defaa496d36453af00fd00958e94bc30c231b4
Laura Artunduaga <laura.perea.artunduaga@gmail.com>
2019-06-03 22:26:29 -0400

    functional testing with mocha

---
+++ test/usersController.test.js
@@ -0,0 +1,37 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+let mongoose = require("mongoose");
+let User = require('../src/models/user/index');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../src/controllers/users');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('users', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        User.remove({}, (err) => { 
+           done();           
+        });        
+    });
+/*
+  * Test the /GET route
+  */
+  describe('/GET user', () => {
+      it('it should GET all the users', (done) => {
+        chai.request(server)
+            .get('/users/')
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('array');
+                  res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+
+});
