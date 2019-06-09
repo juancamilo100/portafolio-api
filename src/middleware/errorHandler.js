4a41c527c1a083de0344ec9b965fb4010cdc6338
JuanEspinosa <juancamilo100@hotmail.com>
2019-06-09 01:27:26 -0400

    Cleanup

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,8 +1,4 @@
 module.exports = (err, req, res, next) => {
-    console.log("error message: ");
-    
-    console.log(err.message);
-    
     res.status(err.status || 500).send({
         message: err.message
     });   
