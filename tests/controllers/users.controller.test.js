e1811defd81eabc4fc04f12c68d58c50ca7a89c6
JuanEspinosa <juancamilo100@hotmail.com>
2019-06-30 22:42:40 -0400

    Added basic test

--- tests/controllers/users.controller.test.js
+++ tests/controllers/users.controller.test.js
@@ -1,2 +1,12 @@
-//During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
+
+import app from "../app";
+import * as request from "supertest";
+
+describe("GET / - a simple api endpoint", () => {
+  it("Hello API Request", async () => {
+    const result = await request(app).get("/");
+    expect(result.text).toEqual("hello");
+    expect(result.statusCode).toEqual(200);
+  });
+});
